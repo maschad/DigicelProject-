c9fc1ad97903a39df0f6fb52c3650dfe7e751bbe
unknown <cn021770@jam-ict-4512.digicelgroup.local>
2015-07-20 15:21:53 -0500

    Not inserting sql error

--- action_page.php
+++ action_page.php
@@ -5,25 +5,26 @@ include 'connection.php';
 // define variables and set to empty values
 $name = $email = $location = $baseStation = $number= $accountNumber ="";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $contactNo = test_input($_POST["contactNo"]);
-  $baseStation = test_input($_POST["baseStation"]);
-  $service = test_input($_POST["service"]);
+if ($_SERVER["REQUEST_METHOD"] == "post") {
 
-  if (empty($_POST["name"])) {
+  $name = test_input($_POST['ContactName']);
+  $email = test_input($_POST['email']);
+  $contactNo = test_input($_POST['contactNo']);
+  $baseStation = test_input($_POST['baseStation']);
+  $service = test_input($_POST['service']);
+
+  if (empty($_POST['ContactName'])) {
     $nameErr = "Name is required";
-	  }  // check if name only contains letters and whitespace
-	if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+  }  // check if name only contains letters and whitespace
+	
+  if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
 	  $nameErr = "Only letters and white space allowed"; 
 	}
-	}
 	else {
-	$name = test_input($_POST["name"]);
+	$name = test_input($_POST['ContactName']);
   }
 
-  if (empty($_POST["email"])) {
+  if (empty($_POST['email'])) {
     $emailErr = "Email is required";
   } 
 	// check if e-mail address is well-formed
@@ -31,38 +32,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	  $emailErr = "Invalid email format"; 
 	}
 	else {
-	$email = test_input($_POST["email"]);
+	$email = test_input($_POST['email']);
 	}
 
-  if (empty($_POST["location"])) {
+  if (empty($_POST['location'])) {
     $location = "Location is required";
   } else {
     $location = test_input($_POST["location"]);
   }
 
-  if (empty($_POST["baseStation"])) {
+  if (empty($_POST['baseStation'])) {
     $comment = "Base Station is required";
   } else {
     $comment = test_input($_POST["baseStation"]);
   }
 
-  if (empty($_POST["contactNo"])) {
+  if (empty($_POST['contactNo'])) {
     $contactErr = "contact Number is required";
   } else {
-    $contact = test_input($_POST["contactNo"]);
+    $contact = test_input($_POST['contactNo']);
   }
 }
 
-$accountNumber = mysql_real_escape_string($_POST['accountNumber']);
 $name = mysql_real_escape_string($_POST['ContactName']);
 $email = mysql_real_escape_string($_POST['email']);
 $location = mysql_real_escape_string($_POST['location']);
 $service = mysql_real_escape_string($_POST['service']);
 $number = mysql_real_escape_string($_POST['contactNo']);
 $baseStation = mysql_real_escape_string($_POST['baseStation']);
+$company = mysql_real_escape_string($_POST['Company']);
+$accountNumber = mysql_real_escape_string($_POST['accountNumber']);
 
-$sql = "INSERT INTO users(AccountNumber,ContactNo,Location,Contact Name, Base Station,Company,Service) values ('$accountNumber',$number','$location','$name','$baseStation','$company')";
-mysql_query($sql);
+$sql = "INSERT INTO users(AccountNumber,[Base Station],Company,[Contact Name],ContactNo,email,Location,Service) values ('$accountNumber','$baseStation','$company','$name','$number','$email','$location','$service')";
+
+if($conn->query($sql) == TRUE){
+  echo "New record creeated successfully";
+}else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 function test_input($data) {
   $data = trim($data);
@@ -70,6 +77,7 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
 $conn->close();
 
 ?>

99a43be822a2a55b98e3c96d4b70e0ac18934d56
Chad <chad.nehemiah94@gmail.com>
2015-08-21 11:58:40 -0500

    Search front end designed

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
     $array = array();
     $con=mysql_connect("localhost","root","");
     $db=mysql_select_db("contacts",$con);
-    $query=mysql_query("select * from users where title LIKE '%{$key}%'");
+    $query=mysql_query("select * from users where [Contact Name] LIKE '%{$key}%'");
     while($row=mysql_fetch_assoc($query))
     {
       $array[] = $row['title'];

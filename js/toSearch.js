3014c189b789fa437c5ec106a36b71ea7bc180ce
Chad <chad.nehemiah94@gmail.com>
2015-08-21 15:29:01 -0500

    search should work but db unpopulated

--- js/toSearch.js
+++ js/toSearch.js
@@ -1,8 +1,29 @@
+$(document).ready(function() {
+ $("#search_input").keyup(function(){
+     var search_input = $(this).val();
+     var dataString = 'keyword='+ search_input;
+     if(search_input.length<=0){
+         $("#searchresultdata").remove();
+     }else{
+         if ( $("#searchresultdata").length ==0 ) {
+             $('<div id="searchresultdata" class="srch-results"></div>').appendTo('.srch-content');
+         }
 
-$(document).ready(function(){
-	$('input.typeahead').typeahead({
-	    name: 'typeahead',
-	    remote:'search.php?key=%QUERY',
-	    limit : 10
-	});
+        //AJAX POST
+        $.ajax({
+             type: "POST",
+             url: "../InputForm/search.php",
+             data: dataString,
+             beforeSend: function() {
+                $('input#search_input').addClass('loading');
+             },
+            success: function(response){
+                 $('#searchresultdata').html(response).show();
+                 if ($('input#search_input').hasClass("loading")) {
+                     $("input#search_input").removeClass("loading");
+                 }
+             }
+         });
+     }
+ });
 });
--- search.php
+++ search.php
@@ -1,12 +1,25 @@
-<?php
-    $key=$_GET['key'];
-    $array = array();
-    $con=mysql_connect("localhost","root","");
-    $db=mysql_select_db("contacts",$con);
-    $query=mysql_query("select * from users where [Contact Name] LIKE '%{$key}%'");
-    while($row=mysql_fetch_assoc($query))
-    {
-      $array[] = $row['title'];
+<?php 
+    
+    require 'connection.php';
+
+  if(isset($_POST['keyword'])){
+     $keyword = trim($_POST['keyword']);
+     $keyword = mysqli_real_escape_string($link, $keyword);
+     $query = "select * from users where [Contact Name] like '$keyword%'"; //MUST BEGIN WITH $KEYWORD
+
+    //echo $query;
+     $result = mysqli_query($link,$query);
+     if($result){
+        if(mysqli_affected_rows($link)!=0){
+         while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+         echo '<span><a class="meterRow">'.$row['meter_name'].'</a><span>'.$row['meter_location'].'</span></span>';
+        }
+     }else {
+        echo 'No Results for :"'.$_POST['keyword'].'"';
+        }
     }
-    echo json_encode($array);
-?>
+    }else {
+     echo 'Parameter Missing';
+    }
+
+ ?>
